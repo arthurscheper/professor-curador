50f38c01da23e3788dbf61083bdd0f9228a7adfe
= <arthur.scheper02@gmail.com>
2025-11-29 18:39:02 -0300

    - Criado listagem e seleção dos blocos e unidades de aprendizagem

--- src/main/java/com/arthurscheper/professorcuradortoolkit/bean/ChatBean.java
+++ src/main/java/com/arthurscheper/professorcuradortoolkit/bean/ChatBean.java
@@ -1,20 +1,25 @@
 package com.arthurscheper.professorcuradortoolkit.bean;
 
 
+import com.arthurscheper.professorcuradortoolkit.model.BlocoDTO;
 import com.arthurscheper.professorcuradortoolkit.model.CursoDTO;
+import com.arthurscheper.professorcuradortoolkit.model.MetadadosCursoDTO;
+import com.arthurscheper.professorcuradortoolkit.model.UnidadeAprendizagemDTO;
 import com.arthurscheper.professorcuradortoolkit.service.GeminiService;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-@RequestScoped
+@ViewScoped
 @Named
 public class ChatBean implements Serializable {
 
@@ -22,12 +27,31 @@ public class ChatBean implements Serializable {
     private GeminiService geminiService;
 
     private CursoDTO cursoDTO;
+    private BlocoDTO blocoDTO;
+    private UnidadeAprendizagemDTO unidadeAprendizagemDTO;
 
     private UploadedFile file;
 
+    private boolean mock;
+
     @PostConstruct
     public void init() {
+        if (mock) {
+            cursoDTO = new CursoDTO();
+            cursoDTO.setMetadadosCurso(new MetadadosCursoDTO());
+            cursoDTO.getMetadadosCurso().setCursoSugerido("Engenharia Mecânica");
+            cursoDTO.setEstrutura(new ArrayList<>());
 
+            blocoDTO = new BlocoDTO();
+            blocoDTO.setTituloBloco("Bloco 1");
+            blocoDTO.setUnidadesAprendizagem(new ArrayList<>());
+
+            unidadeAprendizagemDTO = new UnidadeAprendizagemDTO();
+            unidadeAprendizagemDTO.setTituloUnidadeAprendizagem("Conceitos e Ciclo de Vida da Gestão de Projetos");
+
+            blocoDTO.getUnidadesAprendizagem().add(unidadeAprendizagemDTO);
+            cursoDTO.getEstrutura().add(blocoDTO);
+        }
     }
 
     public void upload() {
@@ -44,6 +68,26 @@ public class ChatBean implements Serializable {
         }
     }
 
+    public void selecionarBloco(BlocoDTO blocoDTO) {
+        this.blocoDTO = blocoDTO;
+    }
+
+    public void selecionarUnidadeAprendizagem(UnidadeAprendizagemDTO unidadeAprendizagemDTO) {
+        this.unidadeAprendizagemDTO = unidadeAprendizagemDTO;
+    }
+
+    public boolean isRenderizarBlocos() {
+        return cursoDTO != null && blocoDTO == null;
+    }
+
+    public boolean isRenderizarUnidadesAprendizagens() {
+        return cursoDTO != null && blocoDTO != null && unidadeAprendizagemDTO == null;
+    }
+
+    public boolean isRenderizarUnidadeAprendizagemSelecionada() {
+        return cursoDTO != null && blocoDTO != null && unidadeAprendizagemDTO != null;
+    }
+
     public UploadedFile getFile() {
         return file;
     }
@@ -58,5 +102,21 @@ public class ChatBean implements Serializable {
 
     public void setCursoDTO(CursoDTO cursoDTO) {
         this.cursoDTO = cursoDTO;
+    }
+
+    public BlocoDTO getBlocoDTO() {
+        return blocoDTO;
+    }
+
+    public void setBlocoDTO(BlocoDTO blocoDTO) {
+        this.blocoDTO = blocoDTO;
+    }
+
+    public UnidadeAprendizagemDTO getUnidadeAprendizagemDTO() {
+        return unidadeAprendizagemDTO;
+    }
+
+    public void setUnidadeAprendizagemDTO(UnidadeAprendizagemDTO unidadeAprendizagemDTO) {
+        this.unidadeAprendizagemDTO = unidadeAprendizagemDTO;
     }
 }
